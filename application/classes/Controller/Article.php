44e905fb4c52a7022cfa7ba29970a4c4f11dd6c1
root <root@palo-test.bc.net>
2013-06-23 19:40:29 +0200

    Commit skeleton blog example kohana 3.3

---
+++ application/classes/Controller/Article.php
@@ -0,0 +1,58 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+ 
+class Controller_Article extends Controller {
+	
+	const INDEX_PAGE = '../../article';
+ 
+    public function action_index() {
+    	
+    	$articles = ORM::factory('Article')->find_all(); // loads all article object from table
+    	 
+    	$view = new View('article/index');
+    	$view->set("articles", $articles); // set "articles" object to view
+    	 
+    	$this->response->body($view);
+    	
+    }
+    
+	// loads the new article form
+    public function action_new() {
+    	$article = new Model_Article();
+    	 
+    	$view = new View('article/edit');
+    	$view->set("article", $article);
+    	 
+    	$this->response->body($view);
+    }
+    
+    // edit the article
+    public function action_edit() {
+    	$article_id = $this->request->param('id');
+    	$article = new Model_Article($article_id);
+    
+    	$view = new View('article/edit');
+    	$view->set("article", $article);
+    	 
+    	$this->response->body($view);
+    }
+    
+    // delete the article
+    public function action_delete() {
+    	$article_id = $this->request->param('id');
+    	$article = new Model_Article($article_id);
+    
+    	$article->delete();
+    	HTTP::redirect(URL::site(self::INDEX_PAGE));
+    }
+    
+    // save the article
+    public function action_post() {
+    	$article_id = $this->request->param('id');
+    	$article = new Model_Article($article_id);
+    	$article->values($_POST); // populate $article object from $_POST array
+    	$article->save(); // saves article to database
+    	 
+    	HTTP::redirect(URL::site(self::INDEX_PAGE));
+    }
+ 
+}
